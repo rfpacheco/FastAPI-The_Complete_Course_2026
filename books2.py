9a159d38e2a279b9dbe81e836b948331bd5ed677
rfpacheco <ronnyfph@gmail.com>
2026-04-28 13:32:28 +0200

    Add `BookRequest` model and update POST endpoint for request validation

--- books2.py
+++ books2.py
@@ -17,6 +17,12 @@ class Book:
         self.description = description
         self.rating = rating
 
+class BookRequest(BaseModel):
+    id: int
+    title: str
+    author: str
+    description: str
+    rating: int
 
 BOOKS = [
     Book(1, 'Computer Science Pro', 'codingwithroby', 'A very nice book!', 5),
@@ -32,5 +38,6 @@ async def read_all_books():
     return BOOKS
 
 @app.post("/create-book")
-async def create_book(book_request=Body()):
-    BOOKS.append(book_request) 
+async def create_book(book_request: BookRequest):
+    new_book = Book(**book_request.model_dump())
+    BOOKS.append(new_book)
