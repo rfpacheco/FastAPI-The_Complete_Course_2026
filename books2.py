95e328b353e72c1ec58ea88f4a18a6e5dc6195ba
rfpacheco <ronnyfph@gmail.com>
2026-04-28 10:57:00 +0200

    Add POST endpoint to create books and include Pydantic for request validation

--- books2.py
+++ books2.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -29,3 +30,7 @@ BOOKS = [
 @app.get("/books")
 async def read_all_books():
     return BOOKS
+
+@app.post("/create-book")
+async def create_book(book_request=Body()):
+    BOOKS.append(book_request) 
