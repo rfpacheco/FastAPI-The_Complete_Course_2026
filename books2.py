60da061a6359f9b6d329cfb8a56496afb5bcb64a
rfpacheco <ronnyfph@gmail.com>
2026-05-01 17:49:40 +0200

    Enhance `BookRequest` model with detailed field descriptions and example JSON schema

--- books2.py
+++ books2.py
@@ -20,11 +20,22 @@ class Book:
         self.rating = rating
 
 class BookRequest(BaseModel):
-    id: Optional[int] = None
+    id: Optional[int] = Field(description='ID is not needed on create', default=None)
     title: str = Field(min_length=3)
     author: str = Field(min_length=1)
     description: str = Field(min_length=1, max_length=100)
     rating: int = Field(gt=0, lt=6)
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "title": "A new book",
+                "author": "codingwithtxuri",
+                "description": "A new description of a book",
+                "rating": 5
+            }
+        }
+    }
 
 BOOKS = [
     Book(1, 'Computer Science Pro', 'codingwithroby', 'A very nice book!', 5),
