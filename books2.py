df3b395e73eea9063faca51a46fc6bcd4a905eb3
rfpacheco <ronnyfph@gmail.com>
2026-05-03 20:49:23 +0200

    Add HTTP 404 handling in GET, PUT, and DELETE endpoints for improved error responses

--- books2.py
+++ books2.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from fastapi import FastAPI, Path, Query
+from fastapi import FastAPI, Path, Query, HTTPException
 from pydantic import BaseModel, Field
 
 app = FastAPI()
@@ -59,6 +59,7 @@ async def read_book(book_id: int = Path(gt=0)):
     for book in BOOKS:
         if book.id == book_id:
             return book
+    raise HTTPException(status_code=404, detail="Book not found")
 
 @app.get("/books/")
 async def read_book_by_rating(book_rating: int = Query(gt=0, lt=6)):
@@ -88,13 +89,23 @@ def find_book_id(book: Book):
 
 @app.put("/update-book/update_book")
 async def update_book(book: BookRequest):
+    book_changed = False
     for i in range(len(BOOKS)):
         if BOOKS[i].id == book.id:
             BOOKS[i] = book
+            book_changed = True
+            break
+    if not book_changed:
+        raise HTTPException(status_code=404, detail="Book not found")
+    return book
 
 @app.delete("/books/{book_id}")
 async def delete_book(book_id: int = Path(gt=0)):
+    book_changed = False
     for i in range(len(BOOKS)):
         if BOOKS[i].id == book_id:
             BOOKS.pop(i)
-            break 
+            book_changed = True
+            break
+    if not book_changed:
+        raise HTTPException(status_code=404, detail="Book not found")
