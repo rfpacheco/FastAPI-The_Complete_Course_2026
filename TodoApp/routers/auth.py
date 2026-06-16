16802b918abdd6dba038a2ec91e7c72c4fc5c304
rfpacheco <ronnyfph@gmail.com>
2026-06-16 23:36:41 +0200

    Refactor `auth.py`: add route prefix and tags, update token URL to `/auth/token`, adjust user creation endpoint path, and improve authentication error handling

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -12,13 +12,16 @@ from passlib.context import CryptContext
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from jose import jwt, JWTError
 
-router = APIRouter()
+router = APIRouter(
+    prefix="/auth",
+    tags=["auth"]
+)
 
 SECRET_KEY = "fe15aa132054afcd38cd02d23bb7f09f49a2ce4c569d1db231c38bc2b0baa4d4"
 ALGORITHM = "HS256"
 
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-oauth2_bearer = OAuth2PasswordBearer(tokenUrl="token")
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl="auth/token")
 
 class CreateUserRequest(BaseModel):
     username: str
@@ -72,7 +75,7 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate user.")
 
 
-@router.post("/auth", status_code=status.HTTP_201_CREATED)
+@router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_user(
     db: db_dependency,
     create_user_request: CreateUserRequest
@@ -98,7 +101,7 @@ async def login_for_access_token(
 ):
     user = authenticate_user(form_data.username, form_data.password, db)
     if not user:
-        return "Failed Authentication"
+        return HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate user.")
     token = create_access_token(user.username, user.id, timedelta(minutes=20))
 
     return {"access_token": token, "token_type": "bearer"}
