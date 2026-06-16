265ab737c51e38296d2a9777fea5440a17f16eff
rfpacheco <ronnyfph@gmail.com>
2026-06-16 20:40:35 +0200

    Add `/token` endpoint for user authentication in `auth.py`, implement `authenticate_user` function, and add `python-multipart` dependency

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -8,6 +8,7 @@ from starlette import status
 from database import SessionLocal
 from models import Users
 from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordRequestForm
 
 router = APIRouter()
 
@@ -34,6 +35,16 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
+def authenticate_user(username: str, password: str, db: db_dependency):
+    user = db.query(Users).filter(Users.username == username).first()
+    if not user:
+        return False
+    if not bcrypt_context.verify(password, user.hashed_password):
+        return False
+    return user
+
+
+
 @router.post("/auth", status_code=status.HTTP_201_CREATED)
 async def create_user(
     db: db_dependency,
@@ -51,3 +62,14 @@ async def create_user(
 
     db.add(create_user_model)
     db.commit()
+
+
+@router.post("/token")
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+    db: db_dependency
+):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        return "Failed Authentication"
+    return "Successful Authentication"
