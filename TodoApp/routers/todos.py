cca594414034e232ec2cf7bed4d21b8fc141e07d
rfpacheco <ronnyfph@gmail.com>
2026-06-17 20:21:52 +0200

    Add `get_current_user` dependency to `/todo` POST endpoint, enforce authentication, and associate todos with the authenticated user

--- TodoApp/routers/todos.py
+++ TodoApp/routers/todos.py
@@ -6,6 +6,7 @@ from models import Todos
 from pydantic import BaseModel, Field
 from sqlalchemy.orm import Session
 from starlette import status
+from .auth import get_current_user
 
 router = APIRouter()
 
@@ -18,6 +19,7 @@ def get_db():
 
 
 db_dependency = Annotated[Session, Depends(get_db)]
+user_dependency = Annotated[dict, Depends(get_current_user)]
 
 
 class TodoRequest(BaseModel):
@@ -41,8 +43,10 @@ async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
 
 
 @router.post("/todo", status_code=status.HTTP_201_CREATED)
-async def create_todo(db: db_dependency, todo_request: TodoRequest):
-    todo_model = Todos(**todo_request.model_dump())
+async def create_todo(user: user_dependency, db: db_dependency, todo_request: TodoRequest):
+    if user is None:
+        raise HTTPException(status_code=401, detail='Authentication Failed')
+    todo_model = Todos(**todo_request.model_dump(), owner_id=user["id"])
 
     db.add(todo_model)
     db.commit()
